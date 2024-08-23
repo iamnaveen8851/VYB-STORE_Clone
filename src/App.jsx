e1661035f6d80d70e62e3a5ae3de475bcd433b1d
iamnaveen8851 <nk24528.nk81@gmail.com>
2024-08-23 16:01:53 +0530

    set up is ready

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Homepage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
